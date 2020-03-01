20ef34d45309bf828bef51a444eddcf896d5173a
zxb888 <zxb3000@gmail.com>
2020-03-01 20:51:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,4 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.252/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.245/g' package/base-files/files/bin/config_generate
